ad43629a8d7354ae7ad3b1d38d5821fff0b0b0eb
Fortrex7 <150760037+Fortrex7@users.noreply.github.com>
2024-08-18 01:38:41 +0500

    Add files via upload

---
+++ Rekursiya/Задание1/Задание1.py
@@ -0,0 +1,10 @@
+﻿my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def rec(n):
+    if n == []:
+        print("Конец списка.")
+    else:
+        print(my_list.pop(0))
+        rec(n)
+
+rec(my_list)
